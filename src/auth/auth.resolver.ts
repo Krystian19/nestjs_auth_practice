5cb9bb56ddbe197e022dab0ed36d3d91d7465278
Jan Guzman <janfrancisco19@gmail.com>
2024-10-13 17:13:36 -0400

    basic operation signup schema

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,7 +1,7 @@
 import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
 import { AuthService } from './auth.service';
 import { Auth } from './entities/auth.entity';
-import { CreateAuthInput } from './dto/create-auth.input';
+import { SignUpInput } from './dto/signup-input';
 import { UpdateAuthInput } from './dto/update-auth.input';
 
 @Resolver(() => Auth)
@@ -9,8 +9,8 @@ export class AuthResolver {
   constructor(private readonly authService: AuthService) {}
 
   @Mutation(() => Auth)
-  createAuth(@Args('createAuthInput') createAuthInput: CreateAuthInput) {
-    return this.authService.create(createAuthInput);
+  signUp(@Args('signUpInput') signUpInput: SignUpInput) {
+    return this.authService.create(signUpInput);
   }
 
   @Query(() => [Auth], { name: 'auth' })
